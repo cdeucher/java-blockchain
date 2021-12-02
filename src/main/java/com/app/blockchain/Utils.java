665ab8096a91043b10ce1c2e1a7b54e9935b37b7
tiagoduarterp <39205027+tiagoduarterp@users.noreply.github.com>
2021-12-01 22:07:54 -0300

    Feature/jbc 6 (#8)
    * Revert "[JAVA] add the first tests"
    
    * [JAVA] alterado nomenclatura metodo de teste BlockChainTest
    
    * [JAVA] Desenvolvido metodo Utils para retornar hash
    
    Co-authored-by: cdeucher <cristianoboell@gmail.com>
    Co-authored-by: cdeucher <cristianoboell@yahoo.com.br>
    Co-authored-by: Vinícius Trainotti <vtrainotti@live.com>

--- src/main/java/com/app/blockchain/Utils.java
+++ src/main/java/com/app/blockchain/Utils.java
@@ -1,7 +1,23 @@
 package com.app.blockchain;
 
+import java.security.MessageDigest;
+
 public class Utils {
     public String applySha256(String toString) {
-        return "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(toString.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+            String hex = null;
+            for (int i = 0; i < hash.length; i++) {
+                hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
     }
 }
